f63e3b8b0c0f0052ed504513bc4e7f5661e1162b
ced <zuger.cedric@gmail.com>
2019-07-21 13:45:07 +0200

    Data parsing done.

--- app/controllers/translates_controller.rb
+++ app/controllers/translates_controller.rb
@@ -4,7 +4,40 @@ class TranslatesController < ApplicationController
   end
 
   def create
+    doc = Nokogiri::HTML( open( params['url'] ) )
 
+    # pp doc
+
+    doc.search('script').each do |script|
+      next if script.children.empty?
+      c = script.children
+
+      washed_text = c.text.gsub( /[\n\t]/, '' )
+
+      m = washed_text.match( /fleet:.({.*}),stock/ )
+      next unless m
+
+      data = JSON.parse( m[1] )
+
+      File.open('tmp/fleet.txt','w') do |f|
+        PP.pp(data,f)
+      end
+
+      data['ships'].each do |ship|
+        p ship['image']
+
+        ship['upgrades'].each do |_, upgrade|
+          next unless upgrade
+          # p upgrade
+          p upgrade['image']
+        end
+      end
+
+      data['squadrons'].each do |squadron|
+        p squadron['image']
+      end
+
+    end
   end
 
 end
--- test/controllers/translate_controller_test.rb
+++ test/controllers/translate_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class TranslateControllerTest < ActionDispatch::IntegrationTest
 
   test 'download test url' do
-
+    post translates_url, params: { url: 'https://armada.ryankingston.com/fleet/53386/' }
   end
 
 end
