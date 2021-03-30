922cc08dc2af05b9fc073caa90de2c8affadde7f
ced <zuger.cedric@gmail.com>
2021-03-30 09:21:53 +0200

    A working version with Sinatra.

---
+++ afp.rb
@@ -0,0 +1,30 @@
+require 'rubygems'
+require 'action_view'
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'haml'
+
+require_relative 'data_reader'
+
+include ActionView::Helpers
+
+get '/' do
+  haml :show
+end
+
+post '/print' do
+  # p params
+
+  dr = DataReader.new.download( params['url'] )
+  # dr = DataReader.new.download( 'https://armada.ryankingston.com/fleet/136865/' )
+
+  @ships_images = dr.ships_images
+
+  @name = dr.name
+  @total = dr.total
+
+  @squadron_ratio = dr.squadron_ratio
+  @squadrons_pics = dr.squadrons_pics
+
+  haml :print
+end
